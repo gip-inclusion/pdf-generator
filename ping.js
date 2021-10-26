5452312d224b0adc40b8b08b87183e31f366632f
Cedric Rossi <cedric@terramater.net>
2021-10-26 16:17:34 +0200

    Fix Chrome launch in /ping endpoint

--- ping.js
+++ ping.js
@@ -1,7 +1,11 @@
 import puppeteer from "puppeteer";
 
 export async function ping() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    product: "chrome",
+    args: ["--no-sandbox"],
+    headless: true,
+  });
 
   await browser.close();
 }
