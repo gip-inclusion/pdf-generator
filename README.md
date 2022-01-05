2f5592f86c0e75b4f811ced56fcc1ed46b9e49d3
Cedric Rossi <cedric@terramater.net>
2022-01-05 15:55:11 +0100

    Generalize service for use outside Dora

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 # dora-pdf
 
-- Installer les hooks de pre-commit:
-  `npm run prepare`
+This Express.js service will serve a PDF generated from an headless Chrome for an arbitrary URL.
+
+## Usage
+
+- To get a PDF named `monpdf.pdf` for the page `http://mon-service.com/mapage`, you have to set the `PAGE_URL_PREFIX` environment variable to `http://mon-service.com` and create a link like:
+
+```
+  <a href="https://<thisservicehostname>/print?page=%2Fmapage&name=monpdf.pdf">Download PDF</a>
+```
+
+- The /ping endpoint can be used as a heartbeat for the service. It will return the string "ok" and a 200 status code.
+
+## Installation on Scalingo
+
+- Create a Scalingo application linked to the repository.
+
+  as concurrency is not managed by this service, you should probably use S or M type containers. More information in the [nodejs-buildpack documentation](https://github.com/Scalingo/nodejs-buildpack#reasonable-defaults-for-concurrency)
+
+- Setup the environnement variables:
+
+  - `STACK` should be set to `heroku-18`
+  - `PAGE_URL_PREFIX` should be set to the prefix of the URL you'll want to print (see `Usage`)
+  - `PDF_NAME` should be the default downloaded PDF filename; it will be used in case the `name` query var is missing
+
+## Future improvements
+
+- The generated PDF should probably be cached
---
+++ src/server.js
@@ -0,0 +1,79 @@
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import tmp from "tmp";
+
+import { genPDF, resetBrowser } from "./print.js";
+import { ping } from "./ping.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  helmet({
+    // HSTS is already managed by Scalingo, and the headers
+    // will conflict if we don't disable it here.
+    hsts: false,
+  })
+);
+
+// /ping endpoint
+// Returns "ok" if the service is up
+app.get("/ping", async (req, res) => {
+  await ping();
+  res.send("ok");
+});
+
+// /print endpoint
+// Query vars:
+// - page: the path of the page we'll generate the PDF from.
+//         Will get appended to the PAGE_URL_PREFIX env var.
+// - name (optional): the name of the downloaded PDF.
+//                    If not supplied, will use the PDF_NAME env var.
+app.get("/print", async (req, res, next) => {
+  const page = req.query.page;
+
+  if (!page) {
+    res.status(500);
+    res.send("Missing <code>?page</code> query var");
+    return;
+  }
+
+  const downloadName = req.query.name || process.env.PDF_NAME || "out.pdf";
+
+  try {
+    console.log(
+      `Generating PDF for page ${page} with prefix ${process.env.PAGE_URL_PREFIX}`
+    );
+    const tmpFile = tmp.fileSync();
+
+    await genPDF(page, tmpFile.name);
+
+    res.download(tmpFile.name, downloadName, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      tmpFile.removeCallback();
+    });
+  } catch (err) {
+    console.error(err);
+    await resetBrowser();
+  }
+});
+
+// Sanity checks
+if (!process.env.PAGE_URL_PREFIX) {
+  throw new Error("PAGE_URL_PREFIX should be set");
+}
+
+const port = process.env.PORT;
+
+if (!process.env.PORT) {
+  throw new Error("PORT should be set");
+}
+
+// Run the server
+app.listen(port, () => {
+  console.log("Listening on", port);
+});
