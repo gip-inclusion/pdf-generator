7451d4abe468261e2d49e3260f283c37482fc457
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2024-01-30 17:18:29 +0100

    add error handling to generatePdfFromHtml

--- src/generatePdfFromHtml.js
+++ src/generatePdfFromHtml.js
@@ -1,5 +1,4 @@
 import { launch } from "puppeteer";
-import crypto from "crypto";
 
 export const logWithRequestId = (requestId, message, error) => {
   console.log(`[${requestId}] - ${message}`);
@@ -10,34 +9,42 @@ export const logWithRequestId = (requestId, message, error) => {
 
 export const generatePdfFromHtml = async (htmlContent, requestId) => {
   logWithRequestId(requestId, "generatePdfFromHtml started");
+
   const browser = await launch({
-    headless: "new",
-    args: ["--no-sandbox"],
+    headless: true,
+    args: ["--no-sandbox", "--disable-dev-shm-usage"],
   });
-
   const page = await browser.newPage();
-  await page.setContent(htmlContent, { waitUntil: "load" });
-  await page.emulateMediaType("print");
+  try {
+    await page.setContent(htmlContent, { waitUntil: "load" });
+    await page.emulateMediaType("print");
 
-  const fileName = `document_${requestId}.pdf`;
-  const base64Pdf = (
-    await page.pdf({
-      path: fileName,
-      margin: {
-        top: "2.5cm",
-        right: "1.5cm",
-        bottom: "2.5cm",
-        left: "1.5cm",
-      },
-      printBackground: true,
-      format: "A4",
-    })
-  ).toString("base64");
+    const fileName = `document_${requestId}.pdf`;
+    const base64Pdf = (
+      await page.pdf({
+        path: fileName,
+        margin: {
+          top: "2.5cm",
+          right: "1.5cm",
+          bottom: "2.5cm",
+          left: "1.5cm",
+        },
+        printBackground: true,
+        format: "A4",
+      })
+    ).toString("base64");
 
-  await page.close();
-  await browser.close();
+    await page.close();
+    await browser.close();
 
-  logWithRequestId(requestId, "generatePdfFromHtml finished");
+    logWithRequestId(requestId, "generatePdfFromHtml finished");
 
-  return base64Pdf;
+    return base64Pdf;
+  } catch (error) {
+    logWithRequestId(requestId, "generatePdfFromHtml FAILED", error);
+    throw error;
+  } finally {
+    await page.close();
+    await browser.close();
+  }
 };
