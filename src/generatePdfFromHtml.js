cd63c2d7151cb9ca762db8c90ab26e12b20001b9
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2024-02-01 11:04:37 +0100

    move start generation log

--- src/generatePdfFromHtml.js
+++ src/generatePdfFromHtml.js
@@ -11,37 +11,38 @@ export const logWithRequestId = (requestId, message, error) => {
   }
 };
 
-export const makeGeneratePdfFromHtml = (browser) => async (htmlContent, requestId) => {
-  logWithRequestId(requestId, "generatePdfFromHtml started");
-  return limiter.schedule(async () => {
-    const page = await browser.newPage();
-    try {
-      await page.setContent(htmlContent, { waitUntil: "load" });
-      await page.emulateMediaType("print");
+export const makeGeneratePdfFromHtml =
+  (browser) => async (htmlContent, requestId) => {
+    return limiter.schedule(async () => {
+      logWithRequestId(requestId, "generatePdfFromHtml started");
+      const page = await browser.newPage();
+      try {
+        await page.setContent(htmlContent, { waitUntil: "load" });
+        await page.emulateMediaType("print");
 
-      const fileName = `document_${requestId}.pdf`;
-      const base64Pdf = (
-        await page.pdf({
-          path: fileName,
-          margin: {
-            top: "2.5cm",
-            right: "1.5cm",
-            bottom: "2.5cm",
-            left: "1.5cm",
-          },
-          printBackground: true,
-          format: "A4",
-        })
-      ).toString("base64");
+        const fileName = `document_${requestId}.pdf`;
+        const base64Pdf = (
+          await page.pdf({
+            path: fileName,
+            margin: {
+              top: "2.5cm",
+              right: "1.5cm",
+              bottom: "2.5cm",
+              left: "1.5cm",
+            },
+            printBackground: true,
+            format: "A4",
+          })
+        ).toString("base64");
 
-      logWithRequestId(requestId, "generatePdfFromHtml finished");
+        logWithRequestId(requestId, "generatePdfFromHtml finished");
 
-      return base64Pdf;
-    } catch (error) {
-      logWithRequestId(requestId, "generatePdfFromHtml FAILED", error);
-      throw error;
-    } finally {
-      await page.close();
-    }
-  });
-};
+        return base64Pdf;
+      } catch (error) {
+        logWithRequestId(requestId, "generatePdfFromHtml FAILED", error);
+        throw error;
+      } finally {
+        await page.close();
+      }
+    });
+  };
